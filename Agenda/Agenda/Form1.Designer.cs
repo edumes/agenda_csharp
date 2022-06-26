9aa1566b5ef38a82c6e70c5bf1aa7f54244ddd2a
edumes <66120423+edumes@users.noreply.github.com>
2022-06-26 00:11:08 -0300

    Add files via upload

---
+++ Agenda/Agenda/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zapDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbamigosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.agendaDataSet = new Agenda.AgendaDataSet();
+            this.tb_amigosTableAdapter = new Agenda.AgendaDataSetTableAdapters.tb_amigosTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.input_nome = new System.Windows.Forms.TextBox();
+            this.input_zap = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbamigosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.agendaDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nomeDataGridViewTextBoxColumn,
+            this.zapDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tbamigosBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(333, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 251);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            // 
+            // zapDataGridViewTextBoxColumn
+            // 
+            this.zapDataGridViewTextBoxColumn.DataPropertyName = "zap";
+            this.zapDataGridViewTextBoxColumn.HeaderText = "zap";
+            this.zapDataGridViewTextBoxColumn.Name = "zapDataGridViewTextBoxColumn";
+            // 
+            // tbamigosBindingSource
+            // 
+            this.tbamigosBindingSource.DataMember = "tb_amigos";
+            this.tbamigosBindingSource.DataSource = this.agendaDataSet;
+            // 
+            // agendaDataSet
+            // 
+            this.agendaDataSet.DataSetName = "AgendaDataSet";
+            this.agendaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tb_amigosTableAdapter
+            // 
+            this.tb_amigosTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "WhatsApp:";
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_cadastrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_cadastrar.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cadastrar.Location = new System.Drawing.Point(34, 236);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(94, 39);
+            this.btn_cadastrar.TabIndex = 3;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = false;
+            this.btn_cadastrar.Click += new System.EventHandler(this.btn_cadastrar_Click);
+            // 
+            // input_nome
+            // 
+            this.input_nome.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_nome.Location = new System.Drawing.Point(93, 81);
+            this.input_nome.Name = "input_nome";
+            this.input_nome.Size = new System.Drawing.Size(100, 25);
+            this.input_nome.TabIndex = 4;
+            // 
+            // input_zap
+            // 
+            this.input_zap.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_zap.Location = new System.Drawing.Point(127, 157);
+            this.input_zap.Name = "input_zap";
+            this.input_zap.Size = new System.Drawing.Size(100, 25);
+            this.input_zap.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(621, 373);
+            this.Controls.Add(this.input_zap);
+            this.Controls.Add(this.input_nome);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbamigosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.agendaDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private AgendaDataSet agendaDataSet;
+        private System.Windows.Forms.BindingSource tbamigosBindingSource;
+        private AgendaDataSetTableAdapters.tb_amigosTableAdapter tb_amigosTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn zapDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.TextBox input_nome;
+        private System.Windows.Forms.TextBox input_zap;
+    }
+}
+
---
+++ Agenda/Agenda/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'agendaDataSet.tb_amigos' table. You can move, or remove it, as needed.
+            this.tb_amigosTableAdapter.Fill(this.agendaDataSet.tb_amigos);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            SqlCeConnection cn = new SqlCeConnection(
+                @"Data Source=C:\Users\DESKTOP-E\Desktop\Agenda\Database\Agenda.sdf"
+                );
+
+            SqlCeCommand cm = new SqlCeCommand(
+                "INSERT INTO tb_amigos(nome, zap)" +
+                " VAlUES (@nome_cliente, @tel_cliente)", cn
+                );
+            cm.Parameters.AddWithValue("@nome_cliente", input_nome.Text);
+            cm.Parameters.AddWithValue("@tel_cliente", input_zap.Text);
+
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+
+            MessageBox.Show("Cadastro realizado com sucesso!");
+        }
+    }
+}
